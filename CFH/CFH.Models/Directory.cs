34d80c6afd5fc05e4e0eee2e464fbf7de523e85f
Todor Dakov <tdd@dekom.bg>
2015-11-14 10:06:46 +0200

    Start create models

---
+++ CFH/CFH.Models/Directory.cs
@@ -0,0 +1,32 @@
+﻿namespace CFH.Models
+{
+    using System.Collections.Generic;
+
+    public class Directory
+    {
+        private ICollection<File> files;
+
+        public Directory()
+        {
+            this.files = new HashSet<File>();
+
+        }
+
+        public int DirId { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual ICollection<File> Files
+        {
+            get
+            {
+                return this.files;
+            }
+
+            set
+            {
+                this.files = value;
+            }
+        }
+    }
+}
---
+++ CFH/CFH.Models/File.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFH.Models
+{
+    public class File
+    {
+        public int FileId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Size { get; set; }
+
+        public string Type { get; set; }
+    }
+}
